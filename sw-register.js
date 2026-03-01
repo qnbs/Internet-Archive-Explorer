680d15231f9dcd66c0168f39a8f98306dd91d999
qnbs <155236708+qnbs@users.noreply.github.com>
2026-03-01 03:21:38 +0000

    Updates

---
+++ sw-register.js
@@ -0,0 +1,36 @@
+(() => {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  const currentScript = document.currentScript;
+  if (!(currentScript instanceof HTMLScriptElement) || !currentScript.src) {
+    return;
+  }
+
+  const scriptUrl = new URL(currentScript.src);
+  const appBase = scriptUrl.pathname.endsWith('/sw-register.js')
+    ? scriptUrl.pathname.replace(/sw-register\.js$/, '')
+    : '/';
+
+  navigator.serviceWorker
+    .register(`${appBase}sw.js`, { scope: appBase })
+    .then((reg) => {
+      reg.addEventListener('updatefound', () => {
+        const newWorker = reg.installing;
+        if (!newWorker) {
+          return;
+        }
+
+        newWorker.addEventListener('statechange', () => {
+          if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+            window.waitingServiceWorker = newWorker;
+            window.dispatchEvent(new CustomEvent('swUpdateReady'));
+          }
+        });
+      });
+    })
+    .catch(() => {
+      // no-op in production
+    });
+})();
